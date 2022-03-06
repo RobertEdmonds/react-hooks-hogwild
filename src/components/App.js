22d436042ba16671dc68a8fa05a7a4f584f708cb
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-06 16:14:24 -0700

    commit tree da6e96f1416249a06ea094eb3a22be98653036f5

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [checkGrease, setCheckGrease] = useState(false)
+	const [isGrease, setIsGrease] = useState(false)
+	console.log("hi")
+	function theGrease(item){
+		setCheckGrease(item)
+	}
+	const handleGrease = hogs.filter(pig => {
+		if(checkGrease === (isGrease? false : true))return true;
+
+		return pig.greased === checkGrease
+	})
+	function showGrease(){
+		setIsGrease(!isGrease)
+	} 
+	
 	return (
 		<div className="App">
-			<Nav />
+			<Nav hogs={handleGrease} theGrease={theGrease} isGrease={isGrease} showGrease={showGrease}/>
 		</div>
 	);
 }
